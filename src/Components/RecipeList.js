8838b49a3f4f46da1925a1feecef4ed969cfa38a
Lizzie <lizziewrab@me.com>
2022-03-30 09:40:39 -0500

    search

--- src/Components/RecipeList.js
+++ src/Components/RecipeList.js
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const RecipeList = ({recipes, name }) => {
     // console.log(recipes)
+    const [search, setSearch] = useState('')
+
+    const recipeSearch = recipes.filter(recipe => {
+      return recipe.name.toLowerCase().includes(search.toLowerCase())
+    })
+  
 
     return ( 
       <div className="recipe-list"> 
       <h2>{name}</h2> 
-        {recipes.map(({name, id, source}) => (
+      <input type="text"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)} />
+        {recipeSearch.map(({name, id, source}) => (
             <div className="recipe-preview" key={id}>
               <Link to={`/recipes/${id}`}>
                 <h2>{name}</h2>
